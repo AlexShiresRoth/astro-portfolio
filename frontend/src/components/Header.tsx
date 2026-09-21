056a9c93b51dd544277c4a1cf88ecae0f544a4cf
Alex Roth <hello@alexr.dev>
2026-09-21 14:47:05 -0400

    fix up design

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,39 +1,41 @@
 import type { Header as HeaderType } from "../types/project";
-import BackgroundShapes from "./Background";
 import Links from "./Links";
 
 const Header = ({ header }: { header: HeaderType }) => {
+  const words = ["designing", "building"];
+  const titleMap = header.title.split(" ").map((word) => {
+    return {
+      word,
+      isHighlighted: words.includes(word.toLowerCase()),
+    };
+  });
   return (
-    <header className="w-full flex flex-col justify-center items-center min-h-screen md:min-h-[85vh] relative overflow-x-hidden">
+    <header className="w-full flex flex-col justify-center items-center pt-20 pb-8 md:py-32 md:pt-40 relative overflow-x-hidden">
       <div className="flex w-11/12 md:w-3/4 max-w-6xl">
         <div className="flex flex-col gap-6 w-full">
           <div className="flex flex-col gap-2">
-            <h1 className="text-left text-6xl md:text-8xl font-firaSans animate-fadeIn2 text-black">
-              {header.title}
+            <h1 className="text-left text-7xl md:text-8xl font-bold font-firaSans animate-fadeIn2 text-black leading-none flex flex-wrap gap-2">
+              {titleMap.map((item) => (
+                <span
+                  key={item.word}
+                  className={item.isHighlighted ? "text-amber-700 italic" : ""}
+                >
+                  {item.word}
+                </span>
+              ))}
             </h1>
           </div>
-          <p className="text-xl text-black/80 animate-fadeIn3">
-            {header.subtitle}
-          </p>
-          <div className="flex flex-col gap-4">
-            {header.contentList?.map((content) => (
-              <p
-                key={content}
-                className="md:text-xl animate-fadeIn3 max-w-2xl text-black/80"
-              >
-                {content}
-              </p>
-            ))}
+          <div>
+            <p className="text-xl text-black/80 max-w-2xl animate-fadeIn3">
+              {header.subtitle}
+            </p>
           </div>
           <div className="flex gap-8 text-black animate-fadeIn4">
             <Links />
           </div>
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 w-full -z-10">
-        <BackgroundShapes />
-      </div>
-      <div className="h-0.5 w-full bg-black/50 bottom-0 absolute left-0 animate-growWidth" />
+      <div className="h-0.5 w-full bg-black/5 bottom-0 absolute left-0 animate-growWidth" />
     </header>
   );
 };
--- frontend/src/components/Project.tsx
+++ frontend/src/components/Project.tsx
@@ -1,4 +1,4 @@
-import { ArrowUpRight, ExternalLink, Lock } from "lucide-react";
+import { ArrowUpRight, ExternalLink } from "lucide-react";
 import { cn } from "../lib/utils";
 import type { Project } from "../types/project";
 
@@ -8,52 +8,31 @@ type Props = {
 
 const ProjectComponent = ({ project }: Props) => {
   return (
-    <div className="flex flex-col border-2 border-black rounded-lg bg-white/60">
-      <div className="p-4 flex justify-between border-b border-black gap-2">
-        {project.sourceCodeLink ? (
+    <div className="flex flex-col">
+      <div className="flex flex-col gap-8 md:gap-12 items-start py-8">
+        <div className="w-full flex items-center justify-between gap-8">
           <a
-            href={project.sourceCodeLink}
+            href={project.liveLink}
             target="__blank"
             rel="noopener noreferrer"
-            className="hover:underline flex items-center gap-1 text-sm"
+            className="flex-1 md:flex hidden w-1/3"
           >
-            Source <ArrowUpRight size={14} />
+            {project.optImage && (
+              <img
+                src={project.optImage?.width(850).url()}
+                alt={project.title}
+                className="w-full"
+              />
+            )}
           </a>
-        ) : (
-          <>
-            <Lock size={16} />
-          </>
-        )}
-        <div className="flex gap-2 items-center">
-          <span className="h-1 w-1 rounded-full bg-black block" />
-          <span className="h-1 w-1 rounded-full bg-black block" />
-          <span className="h-1 w-1 rounded-full bg-black block" />
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row gap-8 md:gap-12 items-start p-4 py-8">
-        <a
-          href={project.liveLink}
-          target="__blank"
-          rel="noopener noreferrer"
-          className="w-full max-w-md"
-        >
-          {project.optImage && (
-            <img
-              src={project.optImage?.width(850).url()}
-              alt={project.title}
-              className="bg-center hover:shadow-none transition-all duration-300 w-full h-full object-cover border-2 border-black shadow-[10px_10px_0px_rgba(0,0,0,1)] rounded-lg"
-            />
-          )}
-        </a>
-        <div className="flex gap-2 w-full text-black">
-          <div className="flex flex-col gap-4 w-full max-w-2xl">
-            <div className="flex items-center justify-between w-full">
+          <div className="flex flex-col gap-8 w-full md:w-2/3">
+            <div className="flex items-center justify-between">
               {project.liveLink ? (
                 <a
                   href={project.liveLink}
                   target="__blank"
                   rel="noopener noreferrer"
-                  className="text-xl hover:underline font-semibold flex items-center gap-1"
+                  className="text-3xl hover:underline hover:text-amber-700 transition-all duration-300 flex items-center gap-1"
                 >
                   {project.title}{" "}
                   <span>
@@ -61,19 +40,58 @@ const ProjectComponent = ({ project }: Props) => {
                   </span>
                 </a>
               ) : (
-                <h2 className="text-xl font-semibold">{project.title}</h2>
+                <h2 className="text-3xl">{project.title}</h2>
+              )}
+              {project.sourceCodeLink && (
+                <a
+                  href={project.sourceCodeLink}
+                  target="__blank"
+                  rel="noopener noreferrer"
+                  className="hover:underline flex items-center gap-1 text-sm"
+                >
+                  Source <ArrowUpRight size={14} />
+                </a>
               )}
             </div>
-            <div className="flex flex-col gap-4">
-              {project.problem && (
+            <a
+              href={project.liveLink}
+              target="__blank"
+              rel="noopener noreferrer"
+              className="w-full md:hidden visible"
+            >
+              {project.optImage && (
+                <img
+                  src={project.optImage?.width(850).url()}
+                  alt={project.title}
+                  className="w-full"
+                />
+              )}
+            </a>
+            <p className="text-black/60">{project.solution}</p>
+            <div
+              className={cn(
+                "flex items-center gap-4 justify-start flex-wrap border-t border-black/5 pt-4",
+              )}
+            >
+              {project.code.map((code) => (
+                <span key={code} className="text-xs md:text-sm text-black/50">
+                  {code}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          {/* {project.problem && (
                 <div className="flex flex-col">
-                  <h2 className="font-bold text-amber-500">Challenge</h2>
+                  <h2 className="font-bold text-amber-700">Challenge</h2>
                   <p className="text-black/90">{project.problem}</p>
                 </div>
               )}
               {project.solution && (
                 <div className="flex flex-col">
-                  <h2 className="font-bold text-amber-500">Solution</h2>
+                  <h2 className="font-bold text-amber-700">Solution</h2>
                   <p className="text-black/90">{project.solution}</p>
                 </div>
               )}
@@ -82,37 +100,23 @@ const ProjectComponent = ({ project }: Props) => {
                   <h2 className="font-bold text-amber-500">Overview</h2>
                   <p className="text-black/90">{project.overview}</p>
                 </div>
-              )}
-              {!!project.contributions && (
-                <div className="flex flex-col">
-                  <h2 className="font-bold text-amber-500">Contributions</h2>
-                  <ul>
-                    {project.contributions.map((contribution) => (
-                      <li
-                        key={contribution}
-                        className="text-black/90 list-disc list-inside"
-                      >
-                        {contribution}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
+              )} */}
+          {/* {!!project.contributions && (
+            <div className="flex flex-col">
+              <h2 className="font-bold text-amber-500">Contributions</h2>
+              <ul>
+                {project.contributions.map((contribution) => (
+                  <li
+                    key={contribution}
+                    className="text-black/90 list-disc list-inside"
+                  >
+                    {contribution}
+                  </li>
+                ))}
+              </ul>
             </div>
-          </div>
+          )} */}
         </div>
-      </div>
-      <div
-        className={cn(
-          "flex items-center gap-2 flex-wrap p-4 border-t border-black",
-          project.code.length > 3 ? "justify-between" : "justify-start",
-        )}
-      >
-        {project.code.map((code) => (
-          <span key={code} className="text-xs md:text-sm text-black/80">
-            {code}
-          </span>
-        ))}
       </div>
     </div>
   );
